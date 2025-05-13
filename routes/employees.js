bf0a66c23793a0d68aa06c7c982729627535228b
NatalieChurch <nataliechurch12@gmail.com>
2025-05-13 17:22:07 -0500

    All but one test passed

---
+++ routes/employees.js
@@ -0,0 +1,46 @@
+import express from "express";
+import employees from "#db/employees";
+
+
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+  res.send(employees);
+});
+
+
+router.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random()*employees.length);
+  res.send(employees[randomIndex]);
+});
+
+
+router.get("/:id", (req, res) => {
+  const {id} = req.params;
+  const employee = employees.find((e) => e.id === +id);
+  if (!employee) {
+    return res.status(404).send("Employee not found");
+  }
+  res.send(employee);
+});
+
+
+router.post("/", (req, res) => {
+  const {name} = req.body;
+  if (!name || typeof name !== "string") {
+    return res.status(400).send("Invalid or missing 'name");
+  }
+
+
+  const newEmployee = {
+    id: employees.length ? employees[employees.length - 1].id + 1 : 1,
+    name, 
+  };
+  employees.push(newEmployee);
+  res.status(201).send(newEmployee);
+});
+
+
+export default router;
+
